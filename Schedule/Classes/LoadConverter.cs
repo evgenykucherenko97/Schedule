9ffdd8f0529d613fd48f6501c3de45964c3fdb95
evgenykucherenko97 <evgenykucherenko97@gmail.com>
2018-04-26 16:22:25 +0300

    added work with practical classes

--- Schedule/Classes/LoadConverter.cs
+++ Schedule/Classes/LoadConverter.cs
@@ -22,7 +22,8 @@ namespace Schedule.Classes
             }
             RegularStudyDayLoadSubjects temp;
             //for lector
-            if (tableDataDTODay.LectionCountPerFirstHalf != null || tableDataDTODay.LectionCountPerSecondHalf != null)
+            if ((tableDataDTODay.LectionCountPerFirstHalf != null || tableDataDTODay.LectionCountPerSecondHalf != null) &&
+                (tableDataDTODay.LectionCountPerFirstHalf != 0 || tableDataDTODay.LectionCountPerSecondHalf != 0))
             {
                 temp = new RegularStudyDayLoadSubjects();
                 temp.Id = Guid.NewGuid();
@@ -62,10 +63,13 @@ namespace Schedule.Classes
                 temp.Term = tableDataDTODay.Term;
                 temp.CourseWork = CourseWork.None;
                 temp.HoursForCourseWork = null;
+                temp.HoursOfWork = tableDataDTODay.LectionCountPerFirstHalf + tableDataDTODay.LectionCountPerSecondHalf;
+                temp.HoursOfWorkAll = temp.HoursOfWork;
                 loads.Add(temp);
             }
 
-            if (tableDataDTODay.LabCountPerFirstHalf != null || tableDataDTODay.LabCountPerSecondHalf != null)
+            if ((tableDataDTODay.LabCountPerFirstHalf != null || tableDataDTODay.LabCountPerSecondHalf != null) &&
+                (tableDataDTODay.LabCountPerFirstHalf != 0 || tableDataDTODay.LabCountPerSecondHalf != 0))
             {
                 foreach (var group in groups)
                 {                    
@@ -81,6 +85,7 @@ namespace Schedule.Classes
                     temp.Groups = new List<Group>();
                     temp.Groups.Add(group);
                     temp.StudentCount = (int)group.StudentCount;
+                    temp.DZ = temp.StudentCount / 2.0 * (tableDataDTODay.RK + tableDataDTODay.RGR + tableDataDTODay.RR);
                     temp.Subject.Name += ", " + group.Name;                    
                     if ((bool)tableDataDTODay.FormControlZach)
                     {
@@ -106,45 +111,88 @@ namespace Schedule.Classes
                 }                
             }
 
-        //    \
-        //    tableDataDTODay.SubjectName;
-        //public string Faculty { get; set; }
-        //public string Caf { get; set; }
-        //public string Speciality { get; set; }
-        //public int Grade { get; set; }
-        //public int Term { get; set; }
-        //public string Groups { get; set; }
-        //public int GroupCount { get; set; }
-        //public int StudentCount { get; set; }
+            if ((tableDataDTODay.PracticeCountPerFirstHalf != null || tableDataDTODay.PracticeCountPerSecondHalf != null) &&
+                (tableDataDTODay.PracticeCountPerFirstHalf != 0 || tableDataDTODay.PracticeCountPerSecondHalf != 0))
+            {
+                foreach (var group in groups)
+                {
+                    temp = new RegularStudyDayLoadSubjects();
+                    temp.Id = Guid.NewGuid();
+                    temp.KindOfClasses = KindOfClasses.Labs;
+                    temp.Subject = new Subject()
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = tableDataDTODay.SubjectName
+                    };
+                    temp.Groups = new List<Group>();
+                    temp.Groups.Add(group);
+                    temp.StudentCount = (int)group.StudentCount;
+                    if (tableDataDTODay.LabCountPerFirstHalf == null || tableDataDTODay.LabCountPerSecondHalf == null)
+                        temp.DZ = temp.StudentCount / 2.0 * (tableDataDTODay.RK + tableDataDTODay.RGR + tableDataDTODay.RR);
+                    temp.Subject.Name += ", " + group.Name;
+                    if ((bool)tableDataDTODay.FormControlZach)
+                    {
+                        temp.FormOfControl = FormOfControl.FormControlZach;
+                    }
+                    if ((bool)tableDataDTODay.FormControlDiv)
+                    {
+                        temp.FormOfControl = FormOfControl.FormControlDiv;
+                    }
+                    if ((bool)tableDataDTODay.FormControlExam)
+                    {
+                        temp.FormOfControl = FormOfControl.FormControlExam;
+                    }
+                    temp.HoursForControl = null;
+                    temp.Cons = null;
+                    temp.SelfWorkHours = tableDataDTODay.SelfWorkHours;
+                    temp.Term = tableDataDTODay.Term;
+                    temp.HoursOfWork = tableDataDTODay.PracticeCountPerFirstHalf + tableDataDTODay.PracticeCountPerSecondHalf;
+                    temp.HoursOfWorkAll = temp.HoursOfWork;
+                    //temp.CourseWork = CourseWork.None;
+                    //temp.HoursForCourseWork = null;
+                    loads.Add(temp);
+                }
+            }
 
-        //public int? LectionCountAWeekPerFirstHalf { get; set; }
-        //public int? LectionCountPerFirstHalf { get; set; }
-        //public int? LabCountAWeekPerFirstHalf { get; set; }
-        //public int? LabCountPerFirstHalf { get; set; }
-        //public int? PracticeCountAWeekPerFirstHalf { get; set; }
-        //public int? PracticeCountPerFirstHalf { get; set; }
+            //    \
+            //    tableDataDTODay.SubjectName;
+            //public string Faculty { get; set; }
+            //public string Caf { get; set; }
+            //public string Speciality { get; set; }
+            //public int Grade { get; set; }
+            //public int Term { get; set; }
+            //public string Groups { get; set; }
+            //public int GroupCount { get; set; }
+            //public int StudentCount { get; set; }
 
-        //public int? LectionCountAWeekPerSecondHalf { get; set; }
-        //public int? LectionCountPerSecondHalf { get; set; }
-        //public int? LabCountAWeekPerSecondHalf { get; set; }
-        //public int? LabCountPerSecondHalf { get; set; }
-        //public int? PracticeCountAWeekPerSecondHalf { get; set; }
-        //public int? PracticeCountPerSecondHalf { get; set; }
+            //public int? LectionCountAWeekPerFirstHalf { get; set; }
+            //public int? LectionCountPerFirstHalf { get; set; }
+            //public int? LabCountAWeekPerFirstHalf { get; set; }
+            //public int? LabCountPerFirstHalf { get; set; }
+            //public int? PracticeCountAWeekPerFirstHalf { get; set; }
+            //public int? PracticeCountPerFirstHalf { get; set; }
 
-        //public int? RGR { get; set; }
-        //public int? RR { get; set; }
-        //public int? RK { get; set; }
-        //public int? CourserWork { get; set; }
-        //public int? CourseProject { get; set; }
+            //public int? LectionCountAWeekPerSecondHalf { get; set; }
+            //public int? LectionCountPerSecondHalf { get; set; }
+            //public int? LabCountAWeekPerSecondHalf { get; set; }
+            //public int? LabCountPerSecondHalf { get; set; }
+            //public int? PracticeCountAWeekPerSecondHalf { get; set; }
+            //public int? PracticeCountPerSecondHalf { get; set; }
 
-        //public bool? FormControlZach { get; set; }
-        //public bool? FormControlDiv { get; set; }
-        //public bool? FormControlExam { get; set; }
+            //public int? RGR { get; set; }
+            //public int? RR { get; set; }
+            //public int? RK { get; set; }
+            //public int? CourserWork { get; set; }
+            //public int? CourseProject { get; set; }
 
-        //public double AllCredits { get; set; }
-        //public double SelfWorkHours { get; set; }
-        //public double StudyLoad { get; set; }
-        //public double Npr { get; set; }
+            //public bool? FormControlZach { get; set; }
+            //public bool? FormControlDiv { get; set; }
+            //public bool? FormControlExam { get; set; }
+
+            //public double AllCredits { get; set; }
+            //public double SelfWorkHours { get; set; }
+            //public double StudyLoad { get; set; }
+            //public double Npr { get; set; }
             return loads;
         }
     }
